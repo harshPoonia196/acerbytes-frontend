3aa30568919c790710917deed76b09fbb017692b
Anand Gupta <anandgupta887@yahoo.com>
2023-09-21 21:09:56 +0530

    Added drawer

---
+++ Components/NavBar/Links.js
@@ -0,0 +1,9 @@
+export const ListOfPageLink = [
+  { label: "Property List", route: "/property-list" },
+  { label: "Property Details", route: "/details" },
+  { label: "Property Research", route: "/research" },
+  { label: "Join now", route: "/join-now" },
+  { label: "Broker", route: "/brokers" },
+  { label: "Enquiries", route: "/enquiries" },
+  { label: "Profile", route: "/profile" },
+];
