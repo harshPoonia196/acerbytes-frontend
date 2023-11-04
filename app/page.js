317bc304ae3e10dcfdcc5d624074758e114cdc72
Anand Gupta <anandgupta887@yahoo.com>
2023-11-04 14:52:29 +0530

    fixes

--- app/page.js
+++ app/page.js
@@ -1,32 +1,101 @@
-import { Typography, Container } from "@mui/material";
+"use client";
+
+import {
+  Box,
+  Typography,
+  Container,
+  Card,
+  Grid,
+  CardActionArea,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+import colors from "styles/theme/colors";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <Container maxWidth="md">
-      <a href="/property-list">Redirect to property list page</a>
-      <br />
-      <a href="/brokers">Redirect to brokers page</a>
-      <br />
-      <a href="/enquiries">Redirect to Enquiries page</a>
-      <Typography variant="h1">h1</Typography>
-      <Typography variant="h2">h2</Typography>
-      <Typography variant="h3">h3</Typography>
-      <Typography variant="h4">h4</Typography>
-      <Typography variant="h5">h5</Typography>
-      <Typography variant="h6">h6</Typography>
-      <Typography variant="subtitle1">subtitle1</Typography>
-      <Typography variant="subtitle2">subtitle2</Typography>
-      <Typography variant="body1">body1</Typography>
-      <Typography variant="body2">body2</Typography>
-      <Typography variant="button" display="block">
-        button
-      </Typography>
-      <Typography variant="caption" display="block">
-        caption
-      </Typography>
-      <Typography variant="overline" display="block">
-        overline
-      </Typography>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          p: 3,
+          py: { xs: 4, sm: 6 },
+          // backgroundColor: "white",
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h2" sx={{ color: "#000" }}>
+          For better decision in real estate
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{ fontWeight: 700, display: { xs: "block", sm: "none" } }}
+        >
+          Research . Consult . Decide
+        </Typography>
+        <Typography
+          variant="h3"
+          sx={{ display: { xs: "none", sm: "block" }, color: colors.DISABLED }}
+        >
+          Research . Consult . Decide
+        </Typography>
+      </Box>
+      <Box>
+        <Grid container spacing={0}>
+          <Grid item xs={3}>
+            <Card>
+              <CardActionArea
+                sx={{ p: 2, textAlign: "center" }}
+                onClick={() => {
+                  router.push("/property-list");
+                }}
+              >
+                <Typography variant="h4">Noida</Typography>
+                <Typography variant="h6">2 Properties</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card>
+              <CardActionArea
+                sx={{ p: 2, textAlign: "center" }}
+                onClick={() => {
+                  router.push("/property-list");
+                }}
+              >
+                <Typography variant="h4">Gurgaon</Typography>
+                <Typography variant="h6">2 Properties</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card>
+              <CardActionArea
+                sx={{ p: 2, textAlign: "center" }}
+                onClick={() => {
+                  router.push("/property-list");
+                }}
+              >
+                <Typography variant="h4">Mumbai</Typography>
+                <Typography variant="h6">2 Properties</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card>
+              <CardActionArea
+                sx={{ p: 2, textAlign: "center" }}
+                onClick={() => {
+                  router.push("/property-list");
+                }}
+              >
+                <Typography variant="h4">Pune</Typography>
+                <Typography variant="h6">2 Properties</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
     </Container>
   );
 }
--- app/property-list/page.js
+++ app/property-list/page.js
@@ -25,133 +25,105 @@ function PropertyList() {
     setAlignment(newAlignment);
   };
   return (
-    <>
-      <Box
-        sx={{
-          p: 2,
-          py: { xs: 4, sm: 6 },
-          backgroundColor: "white",
-          textAlign: "center",
-        }}
-      >
-        <Typography
-          variant="h4"
-          sx={{ fontWeight: 700, display: { xs: "block", sm: "none" } }}
-        >
-          Research.Consult.Decide
-        </Typography>
-        <Typography
-          variant="h3"
-          sx={{ fontWeight: 700, display: { xs: "none", sm: "block" } }}
-        >
-          Research.Consult.Decide
-        </Typography>
-        <Typography variant="h6">Research. Consult. Decide</Typography>
-      </Box>
-      <Card>
-        <Container maxWidth="lg">
-          <Grid
-            container
-            sx={{ display: "flex", flexDirection: "row-reverse" }}
-          >
-            <Grid item xs={12} sm={6}>
-              <Card sx={{ boxShadow: "none" }}>
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30144.970768064195!2d72.8535903!3d19.1899016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b6ee06ebad2b%3A0x9c288235c433657d!2sInfiniti%20Mall!5e0!3m2!1sen!2sin!4v1694174929476!5m2!1sen!2sin"
-                  style={{ border: 0 }}
-                  height="100%"
-                  width="100%"
-                  loading="lazy"
-                />
-              </Card>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Card
-                sx={{
-                  p: 2,
-                  height: "100%",
-                  boxShadow: "none",
-                }}
-              >
-                <Typography variant="h3">Noida</Typography>
-                <Typography variant="caption">
-                  Noida's strategic location, robust infrastructure, and
-                  flourishing business environment have contributed to its
-                  status as a vibrant and attractive real estate destination in
-                  the NCR region.
-                </Typography>
-              </Card>
-            </Grid>
-          </Grid>
-        </Container>
-      </Card>
-      <Container maxWidth="lg">
-        <Grid container spacing={2}>
-          <Grid item xs={6} sm={3}>
-            <Card>
-              <Box
-                sx={{
-                  display: "flex",
-                  flex: 1,
-                  pl: 2,
-                  borderRadius: "8px",
-                }}
-              >
-                <InputBase
-                  placeholder="Search"
-                  type="text"
-                  inputProps={{ "aria-label": "Search..." }}
-                  fullWidth
-                />
-                <IconButton type="submit" aria-label="search">
-                  <SearchIcon />
-                </IconButton>
-              </Box>
+    <Container maxWidth="lg">
+      <Card sx={{ mb: 2 }}>
+        <Grid container sx={{ display: "flex", flexDirection: "row-reverse" }}>
+          <Grid item xs={12} sm={6}>
+            <Card sx={{ boxShadow: "none" }}>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30144.970768064195!2d72.8535903!3d19.1899016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b6ee06ebad2b%3A0x9c288235c433657d!2sInfiniti%20Mall!5e0!3m2!1sen!2sin!4v1694174929476!5m2!1sen!2sin"
+                style={{ border: 0 }}
+                height="100%"
+                width="100%"
+                loading="lazy"
+              />
             </Card>
           </Grid>
-          <Grid item xs={6} sm={3}>
-            <SelectTextFields label="Sort by" />
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <SelectTextFields label="Filter by" />
-          </Grid>
-          <Grid item xs={6} sm={3} sx={{ alignSelf: "center" }}>
-            <ToggleButtonGroup
-              color="primary"
-              value={alignment}
-              exclusive
-              onChange={handleChange}
-              aria-label="Platform"
-              sx={{ display: "flex" }}
-              size="small"
+          <Grid item xs={12} sm={6}>
+            <Card
+              sx={{
+                p: 2,
+                height: "100%",
+                boxShadow: "none",
+              }}
             >
-              <ToggleButton value="asc" sx={{ flex: 1 }}>
-                <ArrowUpwardIcon />
-              </ToggleButton>
-              <ToggleButton value="dec" sx={{ flex: 1 }}>
-                <ArrowDownwardIcon />
-              </ToggleButton>
-            </ToggleButtonGroup>
+              <Typography variant="h3">Noida</Typography>
+              <Typography variant="caption">
+                Noida's strategic location, robust infrastructure, and
+                flourishing business environment have contributed to its status
+                as a vibrant and attractive real estate destination in the NCR
+                region.
+              </Typography>
+            </Card>
           </Grid>
-          <Grid item xs={12}>
-            <Grid container spacing={0.25}>
-              <Grid item xs={12} sm={6} md={12}>
-                <PropertyCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={12}>
-                <PropertyCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={12}>
-                <PropertyCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={12}>
-                <PropertyCard />
-              </Grid>
+        </Grid>
+      </Card>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6} sm={3}>
+          <Card>
+            <Box
+              sx={{
+                display: "flex",
+                flex: 1,
+                pl: 2,
+                borderRadius: "8px",
+              }}
+            >
+              <InputBase
+                placeholder="Search"
+                type="text"
+                inputProps={{ "aria-label": "Search..." }}
+                fullWidth
+              />
+              <IconButton type="submit" aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <SelectTextFields label="Sort by" />
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <SelectTextFields label="Filter by" />
+        </Grid>
+        <Grid item xs={6} sm={3} sx={{ alignSelf: "center" }}>
+          <ToggleButtonGroup
+            color="primary"
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+            aria-label="Platform"
+            sx={{ display: "flex" }}
+            size="small"
+          >
+            <ToggleButton value="asc" sx={{ flex: 1 }}>
+              <ArrowUpwardIcon />
+            </ToggleButton>
+            <ToggleButton value="dec" sx={{ flex: 1 }}>
+              <ArrowDownwardIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={0.25}>
+            <Grid item xs={12} sm={6} md={12}>
+              <PropertyCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={12}>
+              <PropertyCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={12}>
+              <PropertyCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={12}>
+              <PropertyCard />
             </Grid>
           </Grid>
         </Grid>
-      </Container>
-    </>
+      </Grid>
+    </Container>
   );
 }
 
