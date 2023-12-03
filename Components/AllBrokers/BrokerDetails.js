284274972782f767db13ca6d805dd3d1a12f2fca
Anand Gupta <anandgupta887@yahoo.com>
2023-12-04 00:10:22 +0530

    Fixed

--- Components/AllBrokers/BrokerDetails.js
+++ Components/AllBrokers/BrokerDetails.js
@@ -6,86 +6,86 @@ import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 
 
 const labels = {
-    0.5: "Useless",
-    1: "Useless+",
-    1.5: "Poor",
-    2: "Poor+",
-    2.5: "Ok",
-    3: "Ok+",
-    3.5: "Good",
-    4: "Good+",
-    4.5: "Excellent",
-    5: "Excellent+",
-  };
+  0.5: "Useless",
+  1: "Useless+",
+  1.5: "Poor",
+  2: "Poor+",
+  2.5: "Ok",
+  3: "Ok+",
+  3.5: "Good",
+  4: "Good+",
+  4.5: "Excellent",
+  5: "Excellent+",
+};
 
 const BrokerDetails = (props) => {
 
-    const {name} = props
+  const { name } = props
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-      };
-      const handleClose = () => {
-        setAnchorEl(null);
-      };
-  
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
 
   return (
     <>
-       <Box sx={{display:'flex', gap:'1rem', justifyContent:'space-between', flexDirection:{xs:'column', evmd:'row' }}}>
-         <Box>
+      <Box sx={{ display: 'flex', gap: '1rem', justifyContent: 'space-between', flexDirection: { xs: 'column', evmd: 'row' } }}>
+        <Box>
           <Typography variant="h3">{name}</Typography>
-          <Typography sx={{my:'0.3rem'}} variant="body2">{'6133 Rockside Rd #400 , independence, OH'}</Typography>
-          
-          <Box sx={{display:'flex', alignItems:'center', gap:'0.3rem'}}>
-          <Rating
-            name="text-feedback"
-            value={4}
-            readOnly
-            precision={0.5}
-            sx={{ fontSize: "1.25rem" }}
-            emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="small" />}
-          />
-          <Typography variant="body2">
-            {labels[4]}
-          </Typography>
+          <Typography sx={{ my: '4px' }} variant="body2">{'6133 Rockside Rd #400 , independence, OH'}</Typography>
+
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Rating
+              name="text-feedback"
+              value={4}
+              readOnly
+              precision={0.5}
+              sx={{ fontSize: "1.25rem" }}
+              emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="small" />}
+            />
+            <Typography variant="body2" sx={{ ml: 1 }}>
+              {labels[4]}
+            </Typography>
           </Box>
 
-          <Typography sx={{mt:'0.3rem', fontSize: "0.8rem"}} variant="p">{'48 Reviews'}</Typography>
+          <Typography variant="body2">{'48 Reviews'}</Typography>
 
-         </Box>
-         <Box sx={{display:'flex', flexDirection:{xs:'row', evmd:'column'}, justifyContent:'space-between'}}>
-            <Box><Button startIcon={<DriveFileRenameOutlineRoundedIcon />} size='small' variant='contained'>Write a Review</Button></Box>
-            <Box>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: { xs: 'row', evmd: 'column' }, justifyContent: 'space-between' }}>
+          <Box><Button startIcon={<DriveFileRenameOutlineRoundedIcon />} size='small' variant='contained'>Write a Review</Button></Box>
+          <Box>
             <Button
-            size='small'
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            endIcon={<ExpandMoreOutlinedIcon size='small'/>}
-             >
-            Sort By
+              size='small'
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              endIcon={<ExpandMoreOutlinedIcon size='small' />}
+            >
+              Sort By
             </Button>
-                <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                    }}
-                >
-                <MenuItem onClick={handleClose}>Most Helpful</MenuItem>
-                <MenuItem onClick={handleClose}>Recent</MenuItem>
-        
-                </Menu>
-            </Box>
-         </Box>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>Most Helpful</MenuItem>
+              <MenuItem onClick={handleClose}>Recent</MenuItem>
+
+            </Menu>
+          </Box>
+        </Box>
       </Box>
     </>
   )
--- Components/BrokersPage/Dialog/RatingDialog.js
+++ Components/BrokersPage/Dialog/RatingDialog.js
@@ -1,83 +1,76 @@
 import React, { useState } from 'react'
 import {
-    Typography,
-    Box,
-    Button,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    TextField,
-  } from "@mui/material";
-  import DoneIcon from "@mui/icons-material/Done";
+  Typography,
+  Box,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+} from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
 import RatingDetails from './RatingDetails';
-
-  
 
 const RatingDialog = (props) => {
 
-    const {openDialog, setOpenDialog, broker} = props
+  const { openDialog, setOpenDialog, broker } = props
 
-    const [submitReview, setSubmitReview] = useState(false)
-   
- 
-      const handleDialogClose = () => {
-        setOpenDialog(false);
-      }
+  const [submitReview, setSubmitReview] = useState(false)
 
-      const handelReviewSubmit = () => {
-        setSubmitReview(true)
-      }
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  }
 
-
-
+  const handelReviewSubmit = () => {
+    setSubmitReview(true)
+  }
 
   return (
     <>
-        <Dialog
-      sx={{ "& .MuiDialog-paper": { borderRadius: "8px !important" } }}
-      open={openDialog}
-      fullWidth={!submitReview && 'xsm'}
-      onClose={handleDialogClose}>
+      <Dialog
+        sx={{ "& .MuiDialog-paper": { borderRadius: "8px !important" } }}
+        open={openDialog}
+        fullWidth={!submitReview && 'xsm'}
+        onClose={handleDialogClose}>
 
-      <DialogTitle>
-        {!submitReview? 
-        <Typography variant="h4" sx={{ fontWeight: 700 }}>
-         Review for {broker?.name}
-        </Typography> : 
-        
-        <Typography variant="h5" sx={{ fontWeight: 700 }}>
-         Thank You, Your Review has been submitted
-        </Typography> 
+        <DialogTitle>
+          {!submitReview ?
+            <Typography variant="h4" sx={{ fontWeight: 700 }}>
+              Review for {broker?.name}
+            </Typography> :
 
-        }
-      </DialogTitle>
-      <DialogContent>
-      {!submitReview &&
-        <Box sx={{display:'flex', flexDirection:'column', gap:'0.5rem'}}>
+            <Typography variant="h5" sx={{ fontWeight: 700 }}>
+              Thank You, Your Review has been submitted
+            </Typography>
 
-          <RatingDetails key={'Professionalism'} title={'Professionalism'}/>
-          <RatingDetails key={'Communication '} title={'Communication'}/>
-          <RatingDetails key={'Real-estate'} title={'Real estate knowledge'}/>
+          }
+        </DialogTitle>
+        <DialogContent>
+          {!submitReview &&
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
 
-          <Box sx={{}}>
-            <TextField
-            fullWidth
-            variant="standard"
-            label="Comments"
-            multiline
-            maxRows={2} />
-          </Box>
+              <RatingDetails key={'Professionalism'} title={'Professionalism'} />
+              <RatingDetails key={'Communication '} title={'Communication'} />
+              <RatingDetails key={'Real-estate'} title={'Real estate knowledge'} />
 
-          <Box sx={{alignSelf:'end'}}>
-            <Button onClick={handelReviewSubmit} startIcon={<DoneIcon />}
-             variant="contained">Submit</Button>
-          </Box>
+              <Box sx={{}}>
+                <TextField
+                  fullWidth
+                  variant="standard"
+                  label="Comments"
+                  multiline
+                  maxRows={2} />
+              </Box>
 
+              <Box sx={{ alignSelf: 'end' }}>
+                <Button onClick={handelReviewSubmit} startIcon={<DoneIcon />}
+                  variant="contained">Submit</Button>
+              </Box>
 
-        </Box>}
-      </DialogContent>
+            </Box>}
+        </DialogContent>
       </Dialog>
-  
+
     </>
   )
 }
