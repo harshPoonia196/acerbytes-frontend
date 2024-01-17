5674cd5d876dbbf8a3555346bc1fbb082c88b7e0
Jitendra Patidar <jitendraramesh.patidar@galaxyweblinks.in>
2024-01-17 10:37:48 +0530

    Show name and contact on page

--- app/user/profile/page.js
+++ app/user/profile/page.js
@@ -503,6 +503,7 @@ function Profile() {
           profileInfo
         );
         if (response.status == 200) {
+          updateDetailsonLocalStorage(profileInfo);
           showToaterMessages(ToasterMessages.PROFILE_UPDATE_SUCCESS, "success");
         }
       }
@@ -517,6 +518,26 @@ function Profile() {
       setLoading(false);
     }
   };
+
+
+  const updateDetailsonLocalStorage=(info)=>{
+    let userInfo = JSON.parse(localStorage.getItem("userDetails"));
+    userInfo = {
+      ...userInfo,
+      name: {
+        ...userInfo.name,
+        firstName: info?.name?.firstName,
+        lastName: info?.name?.lastName
+      },
+      phone: {
+        ...userInfo.phone,
+        number: info?.phone?.number,
+        countryCode: info?.phone?.countryCode
+      }
+    }
+
+    localStorage.setItem("userDetails", JSON.stringify(userInfo));
+  }
 
   console.log("profle data: ", profileInfo);
 
@@ -548,7 +569,7 @@ function Profile() {
               <Box sx={{ display: "flex" }}>
                 <Box sx={{ flex: 1 }}>
                   <Typography variant="h6" sx={{ fontWeight: 900 }}>
-                    Anand Gupta
+                    {userDetails?.name?.firstName} {userDetails?.name?.lastName}
                   </Typography>
                 </Box>
                 <Box>
@@ -563,14 +584,14 @@ function Profile() {
                   >
                     <CallIcon fontSize="small" sx={{ alignSelf: "center" }} />
                     <Typography variant="h6" sx={{ alignSelf: "center" }}>
-                      +91 8794561234
+                    {userDetails?.phone?.countryCode} {userDetails?.phone?.number}
                     </Typography>
                   </a>
                 </Box>
               </Box>
-              <Typography variant="body1" sx={{ mt: 1 }}>
+              {/* <Typography variant="body1" sx={{ mt: 1 }}>
                 Mumbai
-              </Typography>
+              </Typography> */}
             </Card>
           </Grid>
           <Grid item xs={12}>
