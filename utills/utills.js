640fb9f11fc867a5daa906ce789207a8f3a7c154
Anand Gupta <anandgupta887@yahoo.com>
2024-01-17 22:18:19 +0530

    uncommented utils

--- utills/utills.js
+++ utills/utills.js
@@ -28,20 +28,20 @@ const scopes = {
 };
 
 export const checkUrlAccess = (isLogged, url, redirectUser, role) => {
-    // if (!isLogged && !publicRoutes.includes(url)) {
-    //     redirectUser("/login")
-    // }
-    // if (isLogged) {
-    //     if (url.includes("/admin") && role !== "admin") {
-    //         redirectUser("/")
-    //     }
-    //     else if (url.includes("/user") && role !== "user") {
-    //         redirectUser("/")
-    //     }
-    //     else if (url.includes("/consultant") && role !== "broker") {
-    //         redirectUser("/")
-    //     }
-    // }
+    if (!isLogged && !publicRoutes.includes(url)) {
+        redirectUser("/login")
+    }
+    if (isLogged) {
+        if (url.includes("/admin") && role !== "admin") {
+            redirectUser("/")
+        }
+        else if (url.includes("/user") && role !== "user") {
+            redirectUser("/")
+        }
+        else if (url.includes("/consultant") && role !== "broker") {
+            redirectUser("/")
+        }
+    }
 }
 
 export const matchUserRole = (actualRole, matchingRole) => {
