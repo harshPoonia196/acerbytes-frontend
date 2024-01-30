2d5939230b55b43162209866320378515f2d4bed
Hirendudhat009 <dudhat4744@gmail.com>
2024-01-30 10:50:44 +0530

    axiosInstance and Loading state added

--- api/Property.api.js
+++ api/Property.api.js
@@ -1,43 +1,15 @@
-import axios from "axios";
-const UNIVERSAL_ACCESS_TOKEN = process.env.NEXT_PUBLIC_UNIVERSAL_ACCESS_TOKEN;
-const UNIVERSAL_LIST_API = process.env.NEXT_PUBLIC_UNIVERSAL_LIST_API;
-const GET_ACCESS_TOKEN = process.env.NEXT_PUBLIC_GET_ACCESS_TOKEN;
-
-
-const LOCAL_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
+import axiosInstance from "./AxiosInstance";
 
 
 export const getAllProperty = () => {
-  return new Promise((resolve, reject) => {
-    axios({
-      method: "get",
-      url: `${LOCAL_URL}property/getProperties/`,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => resolve(res.data))
-      .catch((err) => reject(err));
-  });
+  return axiosInstance.get("property/getProperties")
 };
 
 
 export const deleteProperty = (propertyId) => {
-    return new Promise((resolve, reject) => {
-      axios({
-        method: "delete",
-        url: `${LOCAL_URL}property/delete/${propertyId}`,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => resolve(res.data))
-        .catch((err) => reject(err));
-    });
-  };
-  
+  return axiosInstance.delete(`property/delete/${propertyId}`)
+
+};
+
 
 
