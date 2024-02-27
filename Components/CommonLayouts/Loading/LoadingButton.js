a3d9b45e3c2003dc28d85ce85336920ea8d6f8c0
Anand Gupta <anandgupta887@yahoo.com>
2024-02-28 01:05:53 +0530

    Fixed dynamic issue for button

--- Components/CommonLayouts/Loading/LoadingButton.js
+++ Components/CommonLayouts/Loading/LoadingButton.js
@@ -4,29 +4,28 @@ import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
 import SaveIcon from '@mui/icons-material/Save';
 
-const CustomButton = ({ loading,loadingText,ButtonText ,disabled,  onClick,icon,variant, ...props }) => (
+const CustomButton = ({ loading, loadingText, ButtonText, disabled, onClick, icon, variant, ...props }) => (
   <>
     {
-    loading ?
-     
-	<LoadingButton
-  loading={loading}
-  loadingPosition="start"
-  startIcon={icon && icon}
-  variant={variant ? variant : 'contained'}
->
-  loading...
-</LoadingButton>
-     :
-     
-<Button 
-  startIcon={icon}
-  variant="contained"
-  onClick={onClick}
-  disabled={disabled}>
-    {ButtonText}</Button>
-    
-  }
+      loading ?
+
+        <LoadingButton
+          loading={loading}
+          loadingPosition="start"
+          startIcon={icon && icon}
+          variant={variant ? variant : 'contained'}
+        >
+          {loadingText && loading ? loadingText : ButtonText}
+        </LoadingButton>
+        :
+        <Button
+          startIcon={icon}
+          variant={variant ? variant : 'contained'}
+          onClick={onClick}
+          disabled={disabled}>
+          {ButtonText}</Button>
+
+    }
   </>
 );
 
