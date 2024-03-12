3b4481eb1fb78e14ad91256c1c5772af1a7bf679
Hirendudhat009 <dudhat4744@gmail.com>
2024-03-12 18:27:53 +0530

    amount fix

--- utills/CommonFunction.js
+++ utills/CommonFunction.js
@@ -103,7 +103,7 @@ let formatDateAndDaysRemaining = (expiryDate) => {
 
 const formatAmount = (amount) => {
   // const numericAmount = Number(amount.replace(/,/g, ''));
-  const numericAmount = Number(String(points).replace(/,/g, ''));
+  const numericAmount = Number(String(amount).replace(/,/g, ''));
   return new Intl.NumberFormat("en-IN", {
     style: "currency",
     currency: "INR",
