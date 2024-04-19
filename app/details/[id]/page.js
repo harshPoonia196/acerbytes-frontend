686356444ffcb98c0bb04583fb08c59cda7cd602
Hirendudhat009 <dudhat4744@gmail.com>
2024-04-19 14:22:51 +0530

    added openGraph

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -1,26 +1,43 @@
 import React from "react";
 import PropertyDetailsPage from "../PropertyDetailsPage";
 import { notFound } from "next/navigation";
+import Head from "next/head";
 
 export async function generateMetadata({ params }) {
   // read route params
-  const parts = params.id.split('-');
+  const parts = params.id.split("-");
   const paramsId = parts[parts.length - 1];
   // fetch data
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
-  const product = await fetch(`${baseUrl}/property/getProperty/${paramsId}`).then((res) => res.json())
-  if(product.status !== 200){
-    notFound()
-  }return {
-    title: product?.data?.marketing?.tagLine ?? "",
-    description: product?.data?.marketing?.description ?? ""
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const product = await fetch(
+    `${baseUrl}/property/getProperty/${paramsId}`
+  ).then((res) => res.json());
+
+  if (product.status !== 200) {
+    notFound();
   }
+  const title = product?.data?.marketing?.tagLine ?? ""
+  const description = product?.data?.marketing?.description ?? ""
+  const imageUrl = product?.data?.marketing?.image ?? "default-image-url.jpg"
+
+  return {
+    title,
+    description,
+    imageUrl,
+    openGraph: {
+      title,
+      description,
+      images: [imageUrl]
+    }
+  };
 }
 
-function page({ params }) {
-  return( 
-    <PropertyDetailsPage params={params} />
+const page = async ({ params }) => {
+  return (
+    <>
+      <PropertyDetailsPage params={params} />
+    </>
   );
-}
+};
 
 export default page;
