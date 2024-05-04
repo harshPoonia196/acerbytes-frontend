3cd573a1415ac09b085ef8fa9d4b88e8a3df54b2
Deepak Rathaur <deepak.rathaur@prodcrew.in>
2024-05-04 23:20:39 +0530

    Worked on User credit Value

--- utills/Constants.js
+++ utills/Constants.js
@@ -350,7 +350,7 @@ const listOfPropertyDetailsTab = [
   },
 ];
 
-export const showStarFor =    ["RERA approved","CC","OC","Construction Progress","Authority registration","Government Loan","Private Bank loan","Fresh","Resale"]
+export const showStarFor = ["RERA approved", "CC", "OC", "Construction Progress", "Authority registration", "Government Loan", "Private Bank loan", "Fresh", "Resale"]
 
 const reactQueryKey = {
   broker: {
@@ -571,13 +571,19 @@ export const privacyData = [
   },
 ];
 
-export const SORTING  ={
-   asc: 'asc',
-   desc: 'desc'
+export const SORTING = {
+  asc: 'asc',
+  desc: 'desc'
 }
 
-export const LINK  ={
+export const LINK = {
   unique: 'Unique Link',
   consultant: 'Consultant Card',
   acrebytes: 'Acrebytes Property Link',
 }
+
+export const LEADS_TAB = [
+  { value: '', label: 'All' },
+  { value: 'pending', label: 'Pending' },
+  { value: 'reviewed', label: 'Reviewed' }
+]
