3a0f490164b93cbf3ae6d25f23bbd344edf8a67e
Developer85 <pooniaharkesh196@gmail.com>
2024-07-17 21:08:35 +0530

    tem

--- app/user/shortlist/page.js
+++ app/user/shortlist/page.js
@@ -11,9 +11,6 @@ import { DEBOUNCE_TIMER } from "utills/Constants";
 
 function ShortList() {
   console.log(" ===========>");
-  console.log(" ===========>");
-  console.log(" ===========>");
-  console.log(" ===========>");
   const [alignment, setAlignment] = useState("asc");
 
   const [favouriteProperty, setfavouriteProperty] = useState([]);
